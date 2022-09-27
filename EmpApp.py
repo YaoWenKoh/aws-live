5dcb4a39bbb42420ba749966e0095b0edbf67daa
YaoWenKoh <112604970+YaoWenKoh@users.noreply.github.com>
2022-09-28 05:06:42 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -179,7 +179,7 @@ def updateAtt():
     check_out = today.strftime("%H:%M:%S")
     check_out = str(check_out)
 
-    insert_sql = "INSERT INTO attendance VALUES (%s, %s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO attendance VALUES (%i, %s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     try:
